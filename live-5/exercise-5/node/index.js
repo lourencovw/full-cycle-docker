8a31fef32458daa6ab456396c45677322cd8c1ef
Lourenco Werneck <lourencovw@gmail.com>
2023-10-25 03:03:11 -0300

    Fix mysql url in node project

--- live-5/exercise-5/node/index.js
+++ live-5/exercise-5/node/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host: 'pfa-mysql',
+    host: 'mysql',
     user: 'root',
     password: 'root',
     database: 'pfa'
